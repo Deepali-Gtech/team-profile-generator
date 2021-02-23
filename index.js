699f8ddcac2f145ddce98787f7636e71c8cc9b1d
Deepali Garg <deepaligarg.chd@gmail.com>
2021-02-23 14:49:27 -0500

    added all required js files, created classes along with properties and methods

---
+++ index.js
@@ -0,0 +1,7 @@
+const Engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const manager = require("./lib/manager");
+
+
+const rita = new Engineer('Rita', '25', 'abc@hmail', 'D-tech');
+console.log(rita.email);
